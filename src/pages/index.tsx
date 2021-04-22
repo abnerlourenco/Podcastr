874e3c8b90d5ac65ca3bd32b2ec789a273bd0b21
abnerlourenco <abner2010_hm@hotmail.com>
2021-04-22 20:30:26 -0300

    date and duration formatting applied

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
+import { parseISO } from 'date-fns';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
-  // ...
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;  
 }
 
 type HomeProps = {
@@ -15,17 +24,17 @@ type HomeProps = {
 //para atribuir uma tipagem, basta ir na variavel.
 
 export default function Home(props: HomeProps) {
-    return (
-      <div>
-        <h1>Index</h1>
-        <p>{JSON.stringify(props.episodes)}</p>
-      </div>
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
 
-  const { data } = await api('episodes', {
+  const { data } = await api.get('episodes', {
     params: {
       //parametros atribuidos na API 
       //limite de 12 episodios carregados (_limit=12)
@@ -37,10 +46,24 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+const episodes = data.map(episode => {
+  return {
+    id: episode.id,
+    title: episode.title,
+    members: episode.members,
+    thumbnail: episode.thumbnail,
+    publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+    description: episode.description,
+    url: episode.file.url,
+  };
+})
+
   return {
     props: {
-       episodes: data,
-     },
-     revalidate: 60 * 60 * 8,
+      episodes,
+    },
+    revalidate: 60 * 60 * 8,
   }
 }
